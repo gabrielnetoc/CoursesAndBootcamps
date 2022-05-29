193631939f636d70db2de84f55fee6682b05aada
Gabriel <gabriel98123_@hotmail.com>
2022-05-28 21:34:52 -0300

    Iniciando app note_day

---
+++ Cursos/Flutter/note_day/android/app/src/main/kotlin/com/example/note_day/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.note_day
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Cursos/Flutter/note_day/lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Center(
+          child: Text("NoteDay",
+          style: TextStyle(
+            color: Colors.black
+          ),),
+        ),
+      ),
+      body: Container(),
+      
+    );
+  }
+}
